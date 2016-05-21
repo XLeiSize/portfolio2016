780fc2d5e32bec4ef8f5a45fc422bec7878990b9
XLeiSize <lei.xing@hotmail.fr>
2016-05-21 13:57:51 +0200

    UPDATE PROJECT / DESIGN

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,32 +1,63 @@
 import Vue from 'vue'
 import Data from '../../data'
-import App from './app'
+import 'gsap'
 
 export default Vue.extend({
   name: 'Home',
   template: require('./template.html'),
   ready () {
     this.projects = Data.projects
-    this.app = new App()
+    this.currentProject = this.projects[0]
+    document.querySelector('.display-project').style.backgroundImage = 'url(' + this.projects[0].img + ')'
+    // this.app = new App()
   },
   data () {
     return {
-      projects: this.projects
+      projects: this.projects,
+      currentProject: this.currentProject
     }
   },
   methods: {
-    showProject (index) {
-      document.querySelector('.display-project').style.backgroundImage = 'url(' + this.projects[index].img + ')'
-      // this.shownSeason = this.data.seasons[index]
+    previewProject (index) {
+      document.querySelector('.display-project').style.backgroundImage = 'url(' + this.currentProject.img + ')'
     },
-    updateProject () {
+    showProject (index) {
+      if (this.currentProject !== this.projects[index]) {
+        let el = document.querySelector('.project-infos')
+        let tl = new TimelineMax({
+          delay: 0.2
+        })
+        tl.from(el, 0.1, {
+          opacity: 0,
+          y: 200,
+          ease: Power0.easeInOut
+        })
+        tl.to(el, 0.1, {
+          opacity: 1,
+          y: 0,
+          ease: Power0.easeInOut
+        })
+        tl.add(function () {
+          this.currentProject = this.projects[index]
+        })
+        document.querySelector('.display-project').style.backgroundImage = 'url(' + this.projects[index].img + ')'
+        let current = document.querySelectorAll('.projects__item')[index]
+        if (document.querySelector('.projects__item.active')) {
+          document.querySelector('.projects__item.active').classList.remove('active')
+        }
+        current.classList.add('active')
+        this.currentProject = this.projects[index]
+      }
+    },
+    updateMask () {
       let e = window.event
-      let posX = (e.clientX * 0.7) + 'px'
-      let posY = (e.clientY * 0.7) + 'px'
-      document.querySelector('.display-project').style.backgroundPosition = posX + ' ' + posY
+      let posX = (e.clientX - window.innerWidth) + 'px'
+      let posY = (e.clientY - window.innerHeight) + 'px'
+
+      document.querySelector('.mask').style.backgroundPosition = posX + ' ' + posY
     },
     hideProject (index) {
-      document.querySelector('.display-project').style.backgroundImage = 'none'
+      document.querySelector('.display-project').style.backgroundImage = 'url(' + this.currentProject.img + ')'
     }
   }
 })
--- src/data/index.js
+++ src/data/index.js
@@ -1,32 +1,50 @@
 const data = {
   projects: [
     {
-      name: 'Audio Vizualisation',
-      technos: 'Pixi.js',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis cumque, explicabo beatae neque facilis cum. Unde consectetur delectus ullam maiores, voluptatibus enim assumenda repellendus, officiis consequatur dolorum quae, ea est.',
-      link: '',
-      img: '/static/img/bg/audio-vizualisation.jpg'
-    },
-    {
       name: 'Three-D Experiment',
+      category: 'works',
       technos: 'Three.js',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis cumque, explicabo beatae neque facilis cum. Unde consectetur delectus ullam maiores, voluptatibus enim assumenda repellendus, officiis consequatur dolorum quae, ea est.',
+      description: 'This experiment was made during my spare time learning THREE.js',
       link: '',
-      img: '/static/img/bg/three-experiment.jpg'
+      button: 'See the experiment',
+      img: '/static/img/bg/three-experiment.jpg',
+      default: true
     },
     {
-      name: 'Mamba Out',
-      technos: 'Vue.js',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis cumque, explicabo beatae neque facilis cum. Unde consectetur delectus ullam maiores, voluptatibus enim assumenda repellendus, officiis consequatur dolorum quae, ea est.',
-      link: '',
+      name: 'Audio Vizualisation',
+      category: 'works',
+      technos: 'Pixi.js',
+      description: 'One of my first experience with HTML5 Audio API and PIXI. It was made during my first year at Gobelins.',
+      link: 'http://leixing.fr/lab/pixi-audio/',
+      button: 'See the experiment',
       img: '/static/img/bg/audio-vizualisation.jpg'
     },
     {
       name: 'Emeraude',
+      category: 'works',
       technos: 'Symfony2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis cumque, explicabo beatae neque facilis cum. Unde consectetur delectus ullam maiores, voluptatibus enim assumenda repellendus, officiis consequatur dolorum quae, ea est.',
+      description: 'Emeraude is an association I am part of. It\'s goal is to help Paris homeless people by providing a mobile application allowing you to help local humanitarian association in their missions.',
       link: '',
-      img: '/static/img/bg/audio-vizualisation.jpg'
+      button: 'View website',
+      img: '/static/img/bg/emeraude.jpg'
+    },
+    {
+      name: 'Mamba Out',
+      category: 'WIP',
+      technos: 'Vue.js',
+      description: 'I wanted to pay tribute to one of the greatest to ever play the game.',
+      link: '',
+      button: 'View the website',
+      img: '/static/img/bg/mamba-out.jpg'
+    },
+    {
+      name: 'Revelarium',
+      category: 'WIP',
+      technos: 'Unity3D',
+      description: 'Revelarium is my end-year project at Gobelins.',
+      link: '',
+      button: 'View website',
+      img: '/static/img/bg/emeraude.jpg'
     }
   ]
 }
